277bf2e626d14e40cbb865c63eff1d485cebaff8
mstasiowski <mstasiowski787@gmail.com>
2023-03-10 12:28:29 +0100

    lab2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MediumCardComponent } from './medium-card/medium-card.component';
+import { SmallCardComponent } from './small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediumCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
